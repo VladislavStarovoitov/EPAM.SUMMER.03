fbf1af00e22db54e21f836d373871002097437fc
Vladislav Starovoitov <vladislav.starovoitov94@gmail.com>
2016-07-07 13:51:23 +0300

    Task 3

---
+++ EPAM.SUMMER.STAROVOITOV.03/BitInsertion/BitInsertion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitInsertion
+{
+    public static class BitInsertion
+    {
+        private const int BitLength = 30;
+        public static void Insertion(ref int destinationNumber, int sourceNumber, int startPosition, int endPosition)
+        {
+            if (endPosition < startPosition)
+            {
+                throw new ArgumentException();
+            }
+            if (endPosition > BitLength || endPosition < 0 || startPosition < 0)
+            {
+                throw new ArgumentException();
+            }
+            sourceNumber <<= startPosition;
+            sourceNumber &= int.MaxValue >> (BitLength - endPosition);
+            int mask = -1;
+            for (int i = startPosition; i <= endPosition; i++)
+            {
+                mask ^= 1 << i;
+            }
+            destinationNumber &= mask;
+            destinationNumber |= sourceNumber;
+        }
+    }
+}
