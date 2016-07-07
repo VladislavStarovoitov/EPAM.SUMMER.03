1ea7f9bd7d3d238beb2a9f9833bf09e0f12a354f
Vladislav Starovoitov <vladislav.starovoitov94@gmail.com>
2016-07-07 14:22:35 +0300

    Add xml comments

--- EPAM.SUMMER.STAROVOITOV.03/BitInsertion/BitInsertion.cs
+++ EPAM.SUMMER.STAROVOITOV.03/BitInsertion/BitInsertion.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace BitInsertion
 {
+    /// <summary>
+    /// Provides static method that insert one number to another.
+    /// </summary>
     public static class BitInsertion
     {
         private const int BitLength = 30;
+
+        /// <summary>
+        /// Inserts one number to the other starting with the starPosition and ending with the endPosition.
+        /// </summary>
+        /// <param name="destinationNumber">Number in wich sourceNumber will be inserted.</param>
+        /// <param name="sourceNumber">Number that will be inserted.</param>
+        /// <param name="startPosition">Bit position in destinationNumber at wich insertion begins.</param>
+        /// <param name="endPosition">Bit position in destinationNumber at wich insertion ends.</param>
         public static void Insertion(ref int destinationNumber, int sourceNumber, int startPosition, int endPosition)
         {
             if (endPosition < startPosition)
